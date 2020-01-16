6d76ad17ad04e812d5101d2e94c6d5a783f19274
sanjibbehera <51856169+sanjibbehera@users.noreply.github.com>
2020-01-17 00:23:56 +0530

    some additional configuration around docker and adding the nexus installation script.

--- vagrant_docker_centos/centos/install-docker.sh
+++ vagrant_docker_centos/centos/install-docker.sh
@@ -2,4 +2,5 @@
 
 
 curl -fsSL https://get.docker.com -o get-docker.sh \
-    && sudo sh get-docker.sh
+    && sudo sh get-docker.sh \
+    && sudo systemctl start docker && sudo systemctl enable docker
