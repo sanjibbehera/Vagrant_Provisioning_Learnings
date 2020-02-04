68ec887baf0b2ab1e79fa83cab061091cf3e7003
sanjibbehera <51856169+sanjibbehera@users.noreply.github.com>
2020-02-04 17:50:51 +0530

    Update installPostgressDB.sh

--- vagrant_jenkins_centos/centos/installPostgressDB.sh
+++ vagrant_jenkins_centos/centos/installPostgressDB.sh
@@ -12,7 +12,7 @@ sudo groupadd postgres && sudo useradd postgres -g postgres && sudo usermod -aG 
 sudo rpm -Uvh https://yum.postgresql.org/10/redhat/rhel-7-x86_64/pgdg-centos10-10-2.noarch.rpm
 sudo yum install -y postgresql10-server postgresql10
 sudo usermod -aG wheel postgres && echo postgres | sudo passwd --stdin postgres
-sudo mkdir /opt/postgresDB && sudo chown psotgres:postgres /opt/postgresDB
+sudo mkdir /opt/postgresDB && sudo chown postgres:postgres /opt/postgresDB
 sudo -u postgres /usr/pgsql-10/bin/postgresql-10-setup initdb -D /opt/postgresDB
 
 ################
